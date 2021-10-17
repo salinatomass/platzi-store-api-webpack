22a47848664ce6a3c02921c17e559b37c8621c73
Tomás Salina <salinatomass53@gmail.com>
2021-10-17 00:15:13 -0300

    update initialState

--- src/index.ts
+++ src/index.ts
@@ -2,56 +2,53 @@ import * as express from "express";
 import { Request, Response } from "express";
 import cors = require("cors");
 
-const intialState = {
-  cart: [],
-  products: [
-    {
-      id: "1",
-      image: "https://arepa.s3.amazonaws.com/camiseta.png",
-      title: "Camiseta",
-      price: 25,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: "3",
-      image: "https://arepa.s3.amazonaws.com/mug.png",
-      title: "Mug",
-      price: 10,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: "4",
-      image: "https://arepa.s3.amazonaws.com/pin.png",
-      title: "Pin",
-      price: 4,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: "5",
-      image: "https://arepa.s3.amazonaws.com/stickers1.png",
-      title: "Stickers",
-      price: 2,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: "6",
-      image: "https://arepa.s3.amazonaws.com/stickers2.png",
-      title: "Stickers",
-      price: 2,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: "7",
-      image: "https://arepa.s3.amazonaws.com/hoodie.png",
-      title: "Hoodie",
-      price: 35,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-  ],
-};
+const intialState = [
+  {
+    id: "1",
+    image: "https://arepa.s3.amazonaws.com/camiseta.png",
+    title: "Camiseta",
+    price: 25,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    id: "3",
+    image: "https://arepa.s3.amazonaws.com/mug.png",
+    title: "Mug",
+    price: 10,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    id: "4",
+    image: "https://arepa.s3.amazonaws.com/pin.png",
+    title: "Pin",
+    price: 4,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    id: "5",
+    image: "https://arepa.s3.amazonaws.com/stickers1.png",
+    title: "Stickers",
+    price: 2,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    id: "6",
+    image: "https://arepa.s3.amazonaws.com/stickers2.png",
+    title: "Stickers",
+    price: 2,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    id: "7",
+    image: "https://arepa.s3.amazonaws.com/hoodie.png",
+    title: "Hoodie",
+    price: 35,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+];
 
 const app = express();
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 3006;
 
 app.use(cors());
 
